7a351c50b2e41457ea823a46b2297a7b6ed37e4d
Leandro Macedo <leandro.smacedo@Live.com>
2024-02-06 09:50:17 +0000

    Create playground.js

---
+++ src/hooks/playground.js
@@ -0,0 +1,18 @@
+const pessoa = [
+    {
+        nome: "fulano",
+        idade: 20
+    },
+    {
+        nome: "fulano",
+        idade: 20
+    },
+    {
+        nome: "fulano",
+        idade: 20
+    },
+    {
+        nome: "fulano",
+        idade: 20
+    }
+]
