9b90c5972682efc3f3f81bc1c3101040e01918be
Leandro Macedo <leandro.smacedo@Live.com>
2024-02-06 09:38:57 +0000

    first setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { EmojiHappy } from 'iconsax-react';
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-emerald-500"> OLA WORLD</h1>
+    </>
+  )
+}
+
+export default App
