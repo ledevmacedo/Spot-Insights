a06b74f385bb578719031e431378b12a3b60736d
Leandro Macedo <leandro.smacedo@Live.com>
2024-02-06 13:31:38 +0000

    card

--- src/pages/home/totalHoursCard.jsx
+++ src/pages/home/totalHoursCard.jsx
@@ -1,12 +1,12 @@
-import { MusicFilter } from "iconsax-react";
+import { MusicPlay } from "iconsax-react";
 
-export function TotalPlaysCard({ playTime }) {
+export function TotalHoursCard({ playTime }) {
     return (
         <>
             <div className="rounded-lg bg-purple-dark-light p-4 w-3/6">
                 <div className="flex gap-2 items-center">
-                    <MusicFilter size="25" color="#B282FF" variant="Bold" className="mt-1" />
-                    <p className="text-white-main font-semibold text-normal pt-1">Musics Listened</p>
+                    <MusicPlay size="25" color="#B282FF" variant="Bold" className="mt-1" />
+                    <p className="text-white-main font-semibold text-normal pt-1">Hours Listened</p>
                 </div>
 
                 <div className="flex items-center gap-2 justify-center content-center">
