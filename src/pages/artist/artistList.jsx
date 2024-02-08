718e9923cf9fdd047b1d3acea765930ec5701482
Leandro Macedo <leandro.smacedo@Live.com>
2024-02-08 12:07:15 +0000

    fix gap homepage

--- src/pages/artist/artistList.jsx
+++ src/pages/artist/artistList.jsx
@@ -4,7 +4,7 @@ import { MenuFlow } from "../../components/menuFlow";
 export function ArtistList() {
     return (
         <>
-            <div className="w-full rounded-xl bg-purple-dark-light p-4 text-white-main font-2xl">
+            <div className="w-full rounded-xl bg-purple-dark-light text-white-main font-2xl">
                 <div className="flex flex-col gap-2">
                     <MenuFlow />
                     <ListCard listIndex={"1"} listTitle={"SkyFall"} listMinutes={"200"} trackCover={"../src/assets/images/skyfall.jpg"} />
